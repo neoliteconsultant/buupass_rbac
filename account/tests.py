6ee918d3f3ce518b8a36afa6c635513a7b261eb6
Tony Afula Maganga <neoliteconsultant@gmail.com>
2022-01-07 11:01:55 +0300

    change some urls in test cases

--- account/tests.py
+++ account/tests.py
@@ -1,4 +1,4 @@
-
+from unittest import skip
 from django.test import TestCase
 from .models import User, SubUser
 from .status_code import HTTP_201_CREATED, HTTP_200_OK
@@ -24,10 +24,9 @@ class TestRBAC(TestCase):
         self.sub_user.email = "nexus.vanguard@gmail.com"
         self.sub_user.save()
 
-
     def test_user_creation(self):
         data = {'username':'tonys','first_name': 'Tony', 'last_name': 'Sonia', 'email':'tonys@gmail.com','password':'P*LpH3wrUd9'}
-        response = self.client.post('/v1/auth/users', data, content_type= "application/json")
+        response = self.client.post('/auth/user', data, content_type= "application/json")
         json_output = response.json()
 
         self.assertEqual(response.status_code, HTTP_201_CREATED)
@@ -37,8 +36,8 @@ class TestRBAC(TestCase):
         self.assertEqual(json_output['email'], 'tonys@gmail.com')
 
     def test_subuser_creation(self):
-        data = {'user':self.user.id, 'username':'maxwellk','first_name': 'Maxwell', 'last_name': 'Kaxuri', 'email':'maxwell.kaxuri@gmail.com','password':'s=Eju8?iWIt'}
-        response = self.client.post('/v1/auth/users/subusers', data)
+        data = {'user_id':self.user.id, 'username':'maxwellk','first_name': 'Maxwell', 'last_name': 'Kaxuri', 'email':'maxwell.kaxuri@gmail.com','password':'s=Eju8?iWIt'}
+        response = self.client.post('/auth/sub_user', data, content_type= "application/json")
         json_output = response.json()
 
         self.assertEqual(response.status_code, HTTP_201_CREATED)
@@ -47,11 +46,11 @@ class TestRBAC(TestCase):
         self.assertEqual(json_output['last_name'], 'Kaxuri')
         self.assertEqual(json_output['email'], 'maxwell.kaxuri@gmail.com')
 
-   
-        
+     
     def test_get_users(self):
-        response = self.client.get('/v1/auth/users')
+        response = self.client.get('/auth/user')
         json_output = response.json()
+        
     
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertEqual(json_output[0]['username'], 'lewism')
@@ -60,7 +59,7 @@ class TestRBAC(TestCase):
         self.assertEqual(json_output[0]['email'], 'lewis@buupass.com')
 
     def test_get_subusers(self):
-        response = self.client.get('/v1/auth/users/subusers')
+        response = self.client.get('/auth/sub_user')
         json_output = response.json()
 
         self.assertEqual(response.status_code, HTTP_200_OK)
